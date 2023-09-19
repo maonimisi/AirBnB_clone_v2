af6ade927a0a18e24d5e313588e97c7eddc276b7
Sobowale Emmanuel <sobowalegz2@gmail.com>
2023-09-19 19:45:47 +0100

    error

--- models/review.py
+++ models/review.py
@@ -14,5 +14,5 @@ class Review(BaseModel, Base):
     __tablename__ = "reviews"
 
     text = Column(String(1024), nullable=False)
-    place_id = Column(String(60), ForeignKey('user_id'), nullable=False)
+    place_id = Column(String(60), ForeignKey('place_id'), nullable=False)
     user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
