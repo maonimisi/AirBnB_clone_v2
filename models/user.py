521f682013e69e669882e318a47af4bed1ec28b7
Muhammed Abdulrasheed Onimisi <maonimisi>
2023-09-20 14:05:36 +0100

    fixing errors and bugs

--- models/user.py
+++ models/user.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 """This module defines a class User"""
 from models.base_model import BaseModel, Base
-from sqlalchemy import String, Column, DateTime, ForeignKey
+from sqlalchemy import String, Column
 from sqlalchemy.orm import relationship
+from models.place import Place
+from models.review import Review
 
 
 class User(BaseModel, Base):
