f522021fab47fdd5d439faf0318f1504ddea8861
Muhammed Abdulrasheed Onimisi <maonimisi>
2023-09-20 13:55:52 +0100

    fixing errors and bugs

--- models/user.py
+++ models/user.py
@@ -1,25 +1,25 @@
 #!/usr/bin/python3
 """This module defines a class User"""
 from models.base_model import BaseModel, Base
-from sqlalchemy import String, DateTime, Column, ForeignKey
+from sqlalchemy import String, Column
 from sqlalchemy.orm import relationship
 
 
 class User(BaseModel, Base):
     """
     This class defines a user by various attributes
-    Attributess:
-        email - email address
-        password - password for login
-        first name -  first name
-        last name - last name
+    Attributes:
+        email (str): email address (128 characters), can't be null.
+        password (str): password for login (128 characters), can't be null.
+        first_name (str): first name (128 characters), can be null.
+        last_name (str): last name (128 characters), can be null.
     """
     __tablename__ = 'users'
 
     email = Column(String(128), nullable=False)
     password = Column(String(128), nullable=False)
-    first_name = Column(String(128), nullable=False)
-    last_name = Column(String(128), nullable=False)
+    first_name = Column(String(128))
+    last_name = Column(String(128))
 
     places = relationship('Place', backref='user',
                           cascade='all, delete-orphan')
