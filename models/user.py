cdceaa18363c501925f8b2923ecc0d3a83699fe3
Muhammed Abdulrasheed Onimisi <maonimisi>
2023-09-20 14:01:03 +0100

    fixing errors and bugs

--- models/user.py
+++ models/user.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """This module defines a class User"""
 from models.base_model import BaseModel, Base
-from sqlalchemy import String, Column
+from sqlalchemy import String, Column, DateTime, ForeignKey
 from sqlalchemy.orm import relationship
 
 
