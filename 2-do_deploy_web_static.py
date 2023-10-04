2643218973b1e7cbb1642f7903b4b24e55f0e9c3
maonimisi <mabdulrasheed1210@gmail.com>
2023-10-04 09:52:29 -0700

    Fabric script (based on the file 1-pack_web_static.py) that distributes an archive to your web servers

---
+++ 2-do_deploy_web_static.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+Fabric script (based on the file 1-pack_web_static.py) that distributes
+an archive to your web servers, using the function do_deploy
+"""
+
+from fabric.api import put, run, env
+from os.path import exists
+
+env.hosts = ['54.157.172.41', '54.174.165.251']
+
+def do_deploy(archive_path):
+    """distributes an archive to the web servers"""
+    if not exists(archive_path):
+        return False
+    try:
+        file_name = archive_path.split("/")[-1]
+        no_ext = file_name.split(".")[0]
+        path = "/data/web_static/releases/"
+        put(archive_path, '/tmp/')
+        run('mkdir -p {}{}/'.format(path, no_ext))
+        run('tar -xzf /tmp/{} -C {}{}/'.format(file_name, path, no_ext))
+        run('rm /tmp/{}'.format(file_name))
+        run('mv {0}{1}/web_static/* {0}{1}/'.format(path, no_ext))
+        run('rm -rf {}{}/web_static'.format(path, no_ext))
+        run('rm -rf /data/web_static/current')
+        run('ln -s {}{}/ /data/web_static/current'.format(path, no_ext))
+        return True
+    except Exception as e:
+        print(f"Error: {e}")
+        return False
