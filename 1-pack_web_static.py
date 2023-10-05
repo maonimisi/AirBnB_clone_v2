c865e17f442e52e2e464edb1ab9769750373637d
Muhammed Abdulrasheed Onimisi <maonimisi>
2023-10-05 07:39:44 +0100

    Fabric script that generates a .tgz archive from the contents of the web_static folder

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,7 +1,8 @@
-#!/usr/bin/python3
+!/usr/bin/python3
 """
 Fabric script that generates a .tgz archive from the contents
-of the web_static folder of your AirBnB Clone repo, using the function do_pack
+of the web_static folder of your AirBnB Clone repo, using the function
+do_pack
 """
 
 import os
@@ -17,5 +18,4 @@ def do_pack():
     result = local("tar -cvzf {} web_static".format(file_name))
     if result.failed:
         return None
-    else:
-        return file_name
+    return file_name
