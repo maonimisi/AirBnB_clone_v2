6c3c34566f1daa9d93779a05117066cb60f20cfd
Muhammed Abdulrasheed Onimisi <maonimisi>
2023-10-20 14:55:25 +0100

    Python script that display “n is a number” only if n is an integer

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python3
+"""
+Script that starts a Flask web application
+"""
+
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def index():
+    '''
+    Function display “Hello HBNB!”
+    '''
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    '''
+    Function display “HBNB!”
+    '''
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def cisfun(text):
+    '''
+    display “C ” followed by the value of the text variable
+    '''
+    return 'C ' + text.replace('_', ' ')
+
+
+@app.route('/python/', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def pythonisfun(text='is cool'):
+    '''
+    display “Python ”, followed by the value of the text
+    default value of text is “is cool”
+    '''
+    return 'Python ' + text.replace('_', ' ')
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def isitanumber(n):
+    '''
+    display “n is a number” only if n is an integer
+    '''
+    return "{:d} is a number".format(n)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='5000')
